f5b8358cc7da0bf9bc85294e15903e35b88464ec
tz <50454992+tz01x@users.noreply.github.com>
2022-08-30 21:55:26 +0600

    logout-issue-fix

--- Free Media/logout.php
+++ Free Media/logout.php
@@ -5,10 +5,7 @@ session_start();
 if (isset($_SESSION['USER_DATA'])) {
 
     unset($_SESSION['USER_DATA']);
-
-    header("Location:login.php");
-    die;
-} else {
-    echo "not log out";
 }
+
+header("Location:login.php");
 ?>
